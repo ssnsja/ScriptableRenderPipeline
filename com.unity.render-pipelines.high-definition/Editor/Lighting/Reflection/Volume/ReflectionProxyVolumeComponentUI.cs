4d44bf140dec65df2a7a5ba1a38c58ef58e7d49d
Frédéric Vauchelles <fredericv@unity3d.com>
2018-11-16 13:37:45 +0100

    Removed useless region pragmas

--- com.unity.render-pipelines.high-definition/Editor/Lighting/Reflection/Volume/ReflectionProxyVolumeComponentUI.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/Reflection/Volume/ReflectionProxyVolumeComponentUI.cs
@@ -8,18 +8,14 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
 
     class ReflectionProxyVolumeComponentUI : IUpdateable<SerializedReflectionProxyVolumeComponent>
     {
-        #region Inspector
         #pragma warning disable 618
         public static readonly CED.IDrawer Inspector = CED.Action((s, d, o)
             => ProxyVolumeUI.SectionShape.Draw(s.proxyVolume, d.proxyVolume, o));
         #pragma warning restore 618
-        #endregion
 
-        #region Instance
         public ProxyVolumeUI proxyVolume = new ProxyVolumeUI();
 
         public void Update(SerializedReflectionProxyVolumeComponent s)
             => proxyVolume.Update(s.proxyVolume);
-        #endregion
     }
 }

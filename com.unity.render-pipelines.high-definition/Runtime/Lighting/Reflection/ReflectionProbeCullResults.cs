6c31f18428a61c1a574943a71cd9b3f131092f9e
Frédéric Vauchelles <frederic.vauchelles@unity3d.com>
2018-11-22 15:22:06 +0100

    Fix issue when disposing culling group

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/ReflectionProbeCullResults.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/ReflectionProbeCullResults.cs
@@ -48,7 +48,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             for (var i = 0; i < visiblePlanarReflectionProbeCount; ++i)
                 m_VisiblePlanarReflectionProbes[i] = m_Probes[m_PlanarReflectionProbeIndices[i]];
 
-            CullingGroupManager.instance.Free(m_CullingGroup);
             m_CullingGroup = null;
             m_Probes = null;
         }
